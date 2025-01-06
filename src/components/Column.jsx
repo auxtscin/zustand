04cffd99afd7adbef70048157b2ae665f3a563d0
auxtscin <cindyaustin011206@gmail.com>
2025-01-06 11:32:07 +0700

    zustand 

---
+++ src/components/Column.jsx
@@ -0,0 +1,16 @@
+import { useStore } from '../store';
+import './Column.css';
+import Task from './Task';
+
+export default function Column({ state }) {
+  const tasks = useStore(store.tasks.filter(task=>task.state === state)
+);
+  return (
+    <div className="column">
+      <p>{state}</p>
+      {tasks.map((task) => (
+        <Task title={task.title} key={task.title}/>
+      ))}
+    </div>
+  );
+}
